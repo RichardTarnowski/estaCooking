1f568d5769d8d25653b67d745c091a7a6c8599a6
Richard Tarnowski <alutar8264@ieselcaminas.org>
2024-05-19 14:31:48 +0200

    inicio netlify FUNCTIONS

---
+++ functions/app.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\Request;
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/../.env');
+
+$kernel = new App\Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,7 +12,7 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('default/index.html.twig', [
+        return $this->render('index.html.twig', [
             'controller_name' => 'DefaultController',
             'prueba' => 'Hola joder!'
         ]);
