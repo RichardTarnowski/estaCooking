98ec3a759d0efde1047a4eb0857951ed5dfba35c
Richard Tarnowski <alutar8264@ieselcaminas.org>
2024-05-19 15:55:56 +0200

    inicio netlify FUNCTIONS

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,23 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './functions/app.php',
+    entry: './functions/app.js',
     output: {
         path: path.resolve(__dirname, 'dist/functions'),
-        filename: 'app.php',
+        filename: 'app.js',
+        libraryTarget: 'commonjs2'
+    },
+    target: 'node',
+    resolve: {
+        extensions: ['.js', '.php']
     },
     module: {
         rules: [
             {
-                test: /\.php$/,
-                use: 'raw-loader',
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.js', '.php'],
-    },
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
 };
